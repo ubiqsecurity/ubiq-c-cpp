9b8aac3b0ab8aa96bc74c9dde34008d098a6631f
John Tyner <jtyner@ubiqsecurity.com>
2020-09-24 11:10:00 -0700

    simplify the C example

--- src/examples/ubiq_sample.c
+++ src/examples/ubiq_sample.c
@@ -23,140 +23,126 @@
 #include <stdio.h>
 #include <stdint.h>
 
-/*
- * Function to perform encryption/decryption of a single buffer in memory
- *
- * `transform` is a function pointer pointing to either
- * `ubiq_platform_encrypt` or `ubiq_platform_decrypt`, depending on
- * the operation desired.
- *
- * `creds` is a pointer to an object containing the credentials for the
- * Ubiq platform.
- *
- * `ifp` is a stream referring to an open file and positioned at the point
- * at which this function should start reading. The file will be read to the
- * end.
- *
- * `ofp` is a stream referring to an open file and positioned at the point
- * at which this function should start writing.
- *
- * Given the above, the function will read the entire contents of the file
- * referred to by `ifp`, pass that data through the operation specified by
- * `transform`, and write the entire output to `ofs`.
- */
 static
 int
-ubiq_sample_transform_simple(
-    int (* const transform)(
-        const struct ubiq_platform_credentials *,
-        const void *, size_t, void **, size_t *),
+ubiq_sample_piecewise_encrypt(
     const struct ubiq_platform_credentials * const creds,
     FILE * const ifp, FILE * const ofp)
 {
-    void * ibuf, * obuf;
-    size_t ilen, olen;
-    int res;
-
-    /*
-     * determine the size of the input buffer by seeking to the
-     * end of the file and using the resulting offset as the size.
-     */
-    fseek(ifp, 0, SEEK_END);
-    ilen = ftell(ifp);
-    ibuf = malloc(ilen);
-    fseek(ifp, 0, SEEK_SET);
-
-    fread(ibuf, 1, ilen, ifp);
-
-    res = (*transform)(creds, ibuf, ilen, &obuf, &olen);
-    if (res == 0) {
-        fwrite(obuf, 1, olen, ofp);
-        free(obuf);
-    }
-
-    free(ibuf);
-
-    return res;
-}
-
-/*
- * Function to perform encryption/decryption of a file stream in pieces
- *
- * `begin`, `update`, and `end` point to `ubiq_platform_encryption_begin`,
- * `ubiq_platform_encryption_update`, and `ubiq_platform_encryption_end` or
- * their decryption counterparts, respectively, depending on whether the
- * transformation is an encryption or decryption.
- *
- * `ctx` points to a `ubiq_platform_encryption` or `ubiq_platform_decryption`
- * object.
- *
- * `ifp` is a stream referring to an open file and positioned at the point
- * at which this function should start reading. The file will be read to the
- * end.
- *
- * `ofp` is a stream referring to an open file and positioned at the point
- * at which this function should start writing.
- *
- * Given the above, the function will read the contents of the file referred
- * to by `ifp` in pieces, passing each piece through the operation
- * specified by the `begin`, `update`, and `end` function pointers and writing
- * any output to `ofs`. The transform operation is accomplished by first
- * calling `begin()`, followed by repeated calls to `update()` for each piece.
- * When all data has been processed, the function calls `end()` to complete
- * the operation.
- */
-static
-int
-ubiq_sample_transform_piecewise(
-    int (* const begin)(void *, void **, size_t *),
-    int (* const update)(void *, const void *, size_t, void **, size_t *),
-    int (* const end)(void *, void **, size_t *),
-    void * const ctx, FILE * const ifp, FILE * const ofp)
-{
+    struct ubiq_platform_encryption * ctx;
     void * obuf;
     size_t olen;
     int res;
 
-    /*
-     * Start by calling the begin() function. It may produce
-     * some data which needs to be written to the output.
-     */
-
-    res = (*begin)(ctx, &obuf, &olen);
+    res = ubiq_platform_encryption_create(
+        creds, 1 /* want to use the key once */, &ctx);
     if (res == 0) {
-        fwrite(obuf, 1, olen, ofp);
-        free(obuf);
 
         /*
-         * Now read the contents of the input file in pieces,
-         * passing each piece through the transform's update
-         * function and writing the output produced to the
-         * output stream.
+         * Start by calling the begin() function. It may produce
+         * some data which needs to be written to the output.
          */
-        while (!feof(ifp) && res == 0) {
-            uint8_t ibuf[128 * 1024];
-            size_t ilen;
 
-            ilen = fread(ibuf, 1, sizeof(ibuf), ifp);
-
-            res = (*update)(ctx, ibuf, ilen, &obuf, &olen);
-            if (res == 0) {
-                fwrite(obuf, 1, olen, ofp);
-                free(obuf);
-            }
-        }
-
-        /*
-         * Finally, call end() to complete the operation and
-         * write any data produced by the call to the output file
-         */
+        res = ubiq_platform_encryption_begin(ctx, &obuf, &olen);
         if (res == 0) {
-            res = (*end)(ctx, &obuf, &olen);
+            fwrite(obuf, 1, olen, ofp);
+            free(obuf);
+
+            /*
+             * Now read the contents of the input file in pieces,
+             * encrypting each piece via the update function and
+             * writing the output produced to the output stream.
+             */
+            while (!feof(ifp) && res == 0) {
+                uint8_t ibuf[128 * 1024];
+                size_t ilen;
+
+                ilen = fread(ibuf, 1, sizeof(ibuf), ifp);
+
+                res = ubiq_platform_encryption_update(
+                    ctx, ibuf, ilen, &obuf, &olen);
+                if (res == 0) {
+                    fwrite(obuf, 1, olen, ofp);
+                    free(obuf);
+                }
+            }
+
+            /*
+             * Finally, call end() to complete the operation and
+             * write any data produced by the call to the output file
+             */
             if (res == 0) {
-                fwrite(obuf, 1, olen, ofp);
-                free(obuf);
+                res = ubiq_platform_encryption_end(ctx, &obuf, &olen);
+                if (res == 0) {
+                    fwrite(obuf, 1, olen, ofp);
+                    free(obuf);
+                }
             }
         }
+
+        ubiq_platform_encryption_destroy(ctx);
+    }
+
+    return res;
+}
+
+static
+int
+ubiq_sample_piecewise_decrypt(
+    const struct ubiq_platform_credentials * const creds,
+    FILE * const ifp, FILE * const ofp)
+{
+    struct ubiq_platform_decryption * ctx;
+    void * obuf;
+    size_t olen;
+    int res;
+
+    res = ubiq_platform_decryption_create(creds, &ctx);
+    if (res == 0) {
+
+        /*
+         * Start by calling the begin() function. It may produce
+         * some data which needs to be written to the output.
+         */
+
+        res = ubiq_platform_decryption_begin(ctx, &obuf, &olen);
+        if (res == 0) {
+            fwrite(obuf, 1, olen, ofp);
+            free(obuf);
+
+            /*
+             * Now read the contents of the input file in pieces,
+             * decrypting each piece via the update function and
+             * writing the output produced to the output stream.
+             */
+            while (!feof(ifp) && res == 0) {
+                uint8_t ibuf[128 * 1024];
+                size_t ilen;
+
+                ilen = fread(ibuf, 1, sizeof(ibuf), ifp);
+
+                res = ubiq_platform_decryption_update(
+                    ctx, ibuf, ilen, &obuf, &olen);
+                if (res == 0) {
+                    fwrite(obuf, 1, olen, ofp);
+                    free(obuf);
+                }
+            }
+
+            /*
+             * Finally, call end() to complete the operation and
+             * write any data produced by the call to the output file
+             */
+            if (res == 0) {
+                res = ubiq_platform_decryption_end(ctx, &obuf, &olen);
+                if (res == 0) {
+                    fwrite(obuf, 1, olen, ofp);
+                    free(obuf);
+                }
+            }
+        }
+
+        ubiq_platform_decryption_destroy(ctx);
     }
 
     return res;
@@ -252,53 +238,38 @@ int main(const int argc, char * const argv[])
     }
 
     if (method == UBIQ_SAMPLE_METHOD_SIMPLE) {
-        res = ubiq_sample_transform_simple(
-            (mode == UBIQ_SAMPLE_MODE_ENCRYPT) ?
-            &ubiq_platform_encrypt : &ubiq_platform_decrypt,
-            creds, ifp, ofp);
-    } else {
-        int (* begin)(void *, void **, size_t *);
-        int (* update)(void *, const void *, size_t, void **, size_t *);
-        int (* end)(void *, void **, size_t *);
-        int (* destroy)(void *);
-        void * ctx;
+        void * ibuf, * obuf;
+        size_t ilen, olen;
+        int res;
 
         /*
-         * Set the function and context/object pointers to specify
-         * whether the file should be encrypted or decrypted. The
-         * code uses `void` pointers to avoid warnings/errors about
-         * the difference in types between the encryption and
-         * decryption objects.
+         * determine the size of the input buffer by seeking to the
+         * end of the file and using the resulting offset as the size.
          */
+        fseek(ifp, 0, SEEK_END);
+        ilen = ftell(ifp);
+        ibuf = malloc(ilen);
+        fseek(ifp, 0, SEEK_SET);
+
+        fread(ibuf, 1, ilen, ifp);
 
         if (mode == UBIQ_SAMPLE_MODE_ENCRYPT) {
-            begin = (void *)&ubiq_platform_encryption_begin;
-            update = (void *)&ubiq_platform_encryption_update;
-            end = (void *)&ubiq_platform_encryption_end;
-            destroy = (void *)&ubiq_platform_encryption_destroy;
-
-            res = ubiq_platform_encryption_create(
-                creds, 1, (struct ubiq_platform_encryption **)&ctx);
-        } else {
-            begin = (void *)&ubiq_platform_decryption_begin;
-            update = (void *)&ubiq_platform_decryption_update;
-            end = (void *)&ubiq_platform_decryption_end;
-            destroy = (void *)&ubiq_platform_decryption_destroy;
-
-            res = ubiq_platform_decryption_create(
-                creds, (struct ubiq_platform_decryption **)&ctx);
+            res = ubiq_platform_encrypt(creds, ibuf, ilen, &obuf, &olen);
+        } else /* decrypt */ {
+            res = ubiq_platform_decrypt(creds, ibuf, ilen, &obuf, &olen);
         }
 
-        /*
-         * Finally, perform the transform, and destroy the context
-         * object open return from the transform.
-         */
-
         if (res == 0) {
-            res = ubiq_sample_transform_piecewise(
-                begin, update, end, ctx, ifp, ofp);
+            fwrite(obuf, 1, olen, ofp);
+            free(obuf);
+        }
 
-            (*destroy)(ctx);
+        free(ibuf);
+    } else /* piecewise */{
+        if (mode == UBIQ_SAMPLE_MODE_ENCRYPT) {
+            res = ubiq_sample_piecewise_encrypt(creds, ifp, ofp);
+        } else {
+            res = ubiq_sample_piecewise_decrypt(creds, ifp, ofp);
         }
     }
 
