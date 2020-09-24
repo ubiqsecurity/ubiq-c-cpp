262357f41f39903c9c09c7faef4038a18622859d
John Tyner <jtyner@ubiqsecurity.com>
2020-09-24 08:48:49 -0700

    use more specific error values

--- src/lib/decrypt.c
+++ src/lib/decrypt.c
@@ -171,6 +171,118 @@ get_password_callback(char * const buf, const int size,
     return len;
 }
 
+static
+int
+ubiq_platform_parse_new_key(
+    const struct ubiq_platform_rest_handle * const rest,
+    const char * const srsa,
+    char ** const session, char ** const fingerprint,
+    void ** const keybuf, size_t * const keylen)
+{
+    const void * rsp;
+    size_t len;
+    cJSON * json;
+    int res;
+
+    rsp = ubiq_platform_rest_response_content(rest, &len);
+    res = (json = cJSON_ParseWithLength(rsp, len)) ? 0 : INT_MIN;
+
+    if (res == 0) {
+        EVP_PKEY * prvkey;
+        const cJSON * j;
+
+        prvkey = NULL;
+
+        /*
+         * the code below is very similar to the encryption
+         * response handling. see the comments there, and
+         * consider trying to make the code more common.
+         */
+
+        if (res == 0) {
+            j = cJSON_GetObjectItemCaseSensitive(
+                json, "encrypted_private_key");
+            if (cJSON_IsString(j) && j->valuestring != NULL) {
+                BIO * const bp = BIO_new_mem_buf(j->valuestring, -1);
+                prvkey = PEM_read_bio_PrivateKey(
+                    bp, NULL, get_password_callback, (void *)srsa);
+                BIO_free(bp);
+                if (!prvkey) {
+                    res = -EACCES;
+                }
+            } else {
+                res = -EBADMSG;
+            }
+        }
+
+        if (res == 0) {
+            j = cJSON_GetObjectItemCaseSensitive(
+                json, "encryption_session");
+            if (cJSON_IsString(j) && j->valuestring != NULL) {
+                *session = strdup(j->valuestring);
+            } else {
+                res = -EBADMSG;
+            }
+        }
+
+        if (res == 0) {
+            j = cJSON_GetObjectItemCaseSensitive(
+                json, "key_fingerprint");
+            if (cJSON_IsString(j) && j->valuestring != NULL) {
+                *fingerprint = strdup(j->valuestring);
+            } else {
+                res = -EBADMSG;
+            }
+        }
+
+        if (res == 0) {
+            j = cJSON_GetObjectItemCaseSensitive(
+                json, "wrapped_data_key");
+            if (cJSON_IsString(j) && j->valuestring != NULL) {
+                EVP_ENCODE_CTX * ectx;
+                EVP_PKEY_CTX * pctx;
+                void * buf;
+                size_t len;
+                int outl;
+
+                len = strlen(j->valuestring);
+                buf = malloc(len);
+
+                ectx = EVP_ENCODE_CTX_new();
+                EVP_DecodeInit(ectx);
+                EVP_DecodeUpdate(ectx, buf, &outl, j->valuestring, len);
+                len = outl;
+                EVP_DecodeFinal(ectx, (char *)buf + len, &outl);
+                len += outl;
+                EVP_ENCODE_CTX_free(ectx);
+
+                pctx = EVP_PKEY_CTX_new(prvkey, NULL);
+                EVP_PKEY_decrypt_init(pctx);
+                EVP_PKEY_CTX_set_rsa_padding(pctx, RSA_PKCS1_OAEP_PADDING);
+                EVP_PKEY_decrypt(pctx, NULL, keylen, NULL, 0);
+                *keybuf = malloc(*keylen);
+                if (EVP_PKEY_decrypt(
+                        pctx, *keybuf, keylen, buf, len) <= 0) {
+                    res = -EACCES;
+                }
+                EVP_PKEY_CTX_free(pctx);
+
+                free(buf);
+            } else {
+                res = -EBADMSG;
+            }
+        }
+
+        if (prvkey) {
+            EVP_PKEY_free(prvkey);
+        }
+
+        cJSON_Delete(json);
+    }
+
+    return res;
+}
+
 /*
  * send the encrypted data key to the server to
  * be decrypted
@@ -215,109 +327,27 @@ ubiq_platform_decryption_new_key(
     free(enc);
     free(url);
 
-    if (res == 0 &&
-        ubiq_platform_rest_response_code(d->rest) == HTTP_RC_OK) {
-        const void * rsp;
-        size_t len;
+    if (res == 0) {
+        const int rc = ubiq_platform_rest_response_code(d->rest);
 
-        rsp = ubiq_platform_rest_response_content(d->rest, &len);
-        res = (json = cJSON_ParseWithLength(rsp, len)) ? 0 : INT_MIN;
-
-        if (res == 0) {
-            EVP_PKEY * prvkey;
-            const cJSON * j;
-
-            prvkey = NULL;
-
-            /*
-             * the code below is very similar to the encryption
-             * response handling. see the comments there, and
-             * consider trying to make the code more common.
-             */
-
-            if (res == 0) {
-                j = cJSON_GetObjectItemCaseSensitive(
-                    json, "encrypted_private_key");
-                if (cJSON_IsString(j) && j->valuestring != NULL) {
-                    BIO * const bp = BIO_new_mem_buf(j->valuestring, -1);
-                    prvkey = PEM_read_bio_PrivateKey(
-                        bp, NULL, get_password_callback, (void *)d->srsa);
-                    BIO_free(bp);
-                    if (!prvkey) {
-                        res = -1;
-                    }
-                } else {
-                    res = -1;
-                }
-            }
-
-            if (res == 0) {
-                j = cJSON_GetObjectItemCaseSensitive(
-                    json, "encryption_session");
-                if (cJSON_IsString(j) && j->valuestring != NULL) {
-                    d->session = strdup(j->valuestring);
-                } else {
-                    res = -1;
-                }
-            }
-
-            if (res == 0) {
-                j = cJSON_GetObjectItemCaseSensitive(
-                    json, "key_fingerprint");
-                if (cJSON_IsString(j) && j->valuestring != NULL) {
-                    d->key.fingerprint = strdup(j->valuestring);
-                } else {
-                    res = -1;
-                }
-            }
-
-            if (res == 0) {
-                j = cJSON_GetObjectItemCaseSensitive(
-                    json, "wrapped_data_key");
-                if (cJSON_IsString(j) && j->valuestring != NULL) {
-                    EVP_ENCODE_CTX * ectx;
-                    EVP_PKEY_CTX * pctx;
-                    void * buf;
-                    size_t len;
-                    int outl;
-
-                    len = strlen(j->valuestring);
-                    buf = malloc(len);
-
-                    ectx = EVP_ENCODE_CTX_new();
-                    EVP_DecodeInit(ectx);
-                    EVP_DecodeUpdate(ectx, buf, &outl, j->valuestring, len);
-                    len = outl;
-                    EVP_DecodeFinal(ectx, (char *)buf + len, &outl);
-                    len += outl;
-                    EVP_ENCODE_CTX_free(ectx);
-
-                    pctx = EVP_PKEY_CTX_new(prvkey, NULL);
-                    EVP_PKEY_decrypt_init(pctx);
-                    EVP_PKEY_CTX_set_rsa_padding(pctx, RSA_PKCS1_OAEP_PADDING);
-                    EVP_PKEY_decrypt(pctx, NULL, &d->key.raw.len, NULL, 0);
-                    d->key.raw.buf = malloc(d->key.raw.len);
-                    if (EVP_PKEY_decrypt(pctx,
-                                         d->key.raw.buf, &d->key.raw.len,
-                                         buf, len) <= 0) {
-                        res = -1;
-                    }
-                    EVP_PKEY_CTX_free(pctx);
-
-                    free(buf);
-                } else {
-                    res = -1;
-                }
-            }
-
-            if (prvkey) {
-                EVP_PKEY_free(prvkey);
-            }
-
-            cJSON_Delete(json);
+        if (rc == HTTP_RC_OK) {
+            res = ubiq_platform_parse_new_key(
+                d->rest, d->srsa,
+                &d->session, &d->key.fingerprint,
+                &d->key.raw.buf, &d->key.raw.len);
+        } else if (rc == HTTP_RC_UNAUTHORIZED) {
+            /* something's wrong with the credentials */
+            res = -EACCES;
+        } else if (rc >= 400 && rc < 500) {
+            /* something's wrong with the library */
+            res = -EBADMSG;
+        } else if (rc >= 500 && rc < 600) {
+            /* something's wrong with the server */
+            res = -ECONNABORTED;
+        } else {
+            /* something is very wrong somewhere */
+            res = -EPROTO;
         }
-    } else {
-        res = -1;
     }
 
     return res;
@@ -534,7 +564,7 @@ ubiq_platform_decryption_end(
                 *ptlen = outl;
             } else {
                 free(*ptbuf);
-                res = -1;
+                res = INT_MIN;
             }
         }
 
